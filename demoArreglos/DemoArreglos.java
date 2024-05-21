a0b1ee1824e43cd13e57b6e8df43b295e06d350c
Edgar Jesus Lopez Perez <159453307+edgarlpz13@users.noreply.github.com>
2024-05-21 15:00:41 +0000

    actualizacion clase

---
+++ demoArreglos/DemoArreglos.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+
+public class DemoArreglos{ 
+    public void demoArreglos(){ 
+
+        
+        int a[] = new int[10];
+
+        final int N = 20;
+        float [] vector;
+        vector = new float[N];
+
+        for(int i=0;i<0;i++){ 
+            System.out.println("Valor de vector["+i+"]= "+ vector[i]);
+        }
+
+        Scanner entrada = new Scanner(Sistem.in);
+        for(int i=0;i<0;i++ ){ 
+            System.out.println("Introduce el valor del elemento vector["+i+"]= ");
+            vector[i]=entrada.nextFloat();
+        { 
+
+        for(int I=0;i<0;i++){ 
+         System.out.println("Valor de vector["+i+"]= "+ vector[i]);
+        }
+
+        int[]mes = new int[12];
+
+        float salarios[];
+        salarios = new float[25];
+
+        mes[4] = 3;
+        System.out.println(salarios[mes[4]*3]);
+
+        double []v = new double[15];
+        System.out.println(v.length);
+
+        
+
+    
+
+
+
+
+    }
+}
